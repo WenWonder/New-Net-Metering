46803d6c97fcbe5e0b4cb540c106505883dabfde
WenWonder <117896758+WenWonder@users.noreply.github.com>
2025-08-04 17:55:32 +0800

    Update NM-monitoring.py

--- NM-monitoring.py
+++ NM-monitoring.py
@@ -2,3 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.write('Net Metering Monitoring')
+
+url = 'https://raw.githubusercontent.com/WenWonder/New-Net-Metering/refs/heads/main/sample.csv'
+df = pd.read_csv(url, index_col=0)
+print(df.head(5))
