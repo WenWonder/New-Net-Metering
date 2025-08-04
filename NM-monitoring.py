52d79298e14d7fb1486293f1a6c8d1b40d78526c
WenWonder <117896758+WenWonder@users.noreply.github.com>
2025-08-04 17:57:55 +0800

    Update NM-monitoring.py

--- NM-monitoring.py
+++ NM-monitoring.py
@@ -6,3 +6,5 @@ st.write('Net Metering Monitoring')
 url = 'https://raw.githubusercontent.com/WenWonder/New-Net-Metering/refs/heads/main/sample.csv'
 df = pd.read_csv(url, index_col=0)
 print (df.head(5))
+
+st.write(url)
